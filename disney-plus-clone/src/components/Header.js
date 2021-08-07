98a3537bf9ad619d61a5575dbe971db9cd5e6dd4
Suraj Raj <srraaz1998@gmail.com>
2021-08-07 20:48:49 +0530

    Added header

--- disney-plus-clone/src/components/Header.js
+++ disney-plus-clone/src/components/Header.js
@@ -5,8 +5,32 @@ function Header() {
         <Nav>
             <Logo src="/images/logo.svg"/>
             <NavMenu>
-
+                <a>
+                    <img src="/images/home-icon.svg"/>
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg"/>
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg"/>
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg"/>
+                    <span>ORIGINAL</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg"/>
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg"/>
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
+            <UserImg src="/dp.png"/>
         </Nav>
     )
 }
@@ -30,4 +54,47 @@ const Logo =styled.img`
 
 
 const NavMenu=styled.div`
+    display:flex;
+    flex:1;
+    margin-left:25px;
+    align-items:center;
+    a{
+        display:flex;
+        align-items:center;
+        padding:0 12px;
+        cursor:pointer;
+        img{
+            height:20px;
+        }
+        span{
+            font-size:13px;
+            letter-spacing:1.42px;
+            position:relative;
+            &:after{
+                content:"";
+                height:2px;
+                background:white;
+                position:absolute;
+                left:0;
+                right:0;
+                bottom:-6px;
+                opacity:0;
+                tranform-origin:left center;
+                tranform:scaleX(0);
+                transition:all 250ms cubic-bazier(0.25,0.46,0.45,0.94) 0s;
+            }
+        }
+        &:hover{
+            span:after{
+                tranform:scaleX(1);
+                opacity:1;
+            }
+        }
+    }
+`
+
+const UserImg=styled.img`
+     height:50px;
+     border-radius:50%;
+     cursor:pointer; 
 `
